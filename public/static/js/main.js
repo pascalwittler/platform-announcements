7434e0c20495ee36789c07be35c4f4f615c9d672
Pascal Wittler <git@pascalwittler.de>
2024-10-20 13:15:47 +0200

    Fill sounds datalist options via Javascript

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -4,6 +4,30 @@ class PlatformAnnouncementsInterface {
     this.formDomElement = document.querySelector('form');
     this.soundsDomElement = document.querySelector('.sounds');
     this.soundsSequence = [];
+
+    this.initialize();
+  }
+
+  initialize() {
+    this.fillSoundOptions();
+  }
+
+  fillSoundOptions() {
+    fetch('../../data/sounds.json')
+      .then((response) => response.json())
+      .then((jsonData) => jsonData.sounds)
+      .then((sounds) => {
+        sounds.forEach((sound) => {
+          let option = document.createElement('option');
+          option.value = JSON.stringify(sound);
+          option.dataset.file = `data/sounds/${sound.file}`;
+          option.dataset.text = sound.text;
+          option.dataset.category = sound.category;
+          option.innerText = sound.text;
+
+          this.soundsDomElement.appendChild(option);
+        });
+      });
   }
 }
 
