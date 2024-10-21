fda29624e1b69daaff9e86bcc45c5b0c02289f5f
Pascal Wittler <git@pascalwittler.de>
2024-10-21 23:34:06 +0200

    Remove sounds sequence children data attributes

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -41,9 +41,6 @@ class PlatformAnnouncementsInterface {
         sounds.forEach((sound) => {
           let option = document.createElement('option');
           option.value = JSON.stringify(sound);
-          option.dataset.file = `data/sounds/${sound.file}`;
-          option.dataset.text = sound.text;
-          option.dataset.category = sound.category;
           option.innerText = sound.text;
 
           this.soundsDomElement.appendChild(option);
